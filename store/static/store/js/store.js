8df449f55426475fc6398982318b37d19d64181c
Rubel Biswas <RubelBiswas36@gmail.com>
2021-07-31 23:32:55 +0600

    added cod and op method

---
+++ store/static/store/js/store.js
@@ -0,0 +1,9 @@
+// $(document).ready(function() {
+//     $("#online-payment").on("click", function() {
+//         var $url = 'this.dataset.url';
+//       $('#payment-method').load(" url #method-cod",
+//               function () {
+//                 alert("Load was performed.");
+//               });
+//             });
+//   });
