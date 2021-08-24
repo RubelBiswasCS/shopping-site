52d6334de1029ceae70e64a49a00732c3db7202d
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-08-24 16:20:55 +0600

    modified dashboard style

--- store/views.py
+++ store/views.py
@@ -273,3 +273,13 @@ class ProductListView(ListView):
         # context['now'] = timezone.now()
         return context
 
+class OrderListView(ListView):
+
+    model = Order
+    # paginate_by = 100  # if pagination is desired
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['now'] = timezone.now()
+        return context
+
