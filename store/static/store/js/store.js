885b3714b0219e604d51a0452f9a92dd4ee8efc4
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-09-02 21:29:51 +0600

    modified store.js

--- store/static/store/js/store.js
+++ store/static/store/js/store.js
@@ -7,3 +7,38 @@
 //               });
 //             });
 //   });
+
+
+//fuction for add item to the cart
+var addToCart = function(args) {
+
+          
+           $.ajax({
+               method:"POST",
+               url: args.url,
+               data:{
+                    user_id : args.user_id,
+                    product_id : args.product_id,
+                    csrfmiddlewaretoken: args. csrfmiddlewaretoken,
+    
+                    
+               },
+               
+            success: function(response){
+                var myObj = JSON.parse(response);
+                console.log(myObj.user_id+' '+myObj.product_id);
+                console.log('data successfully send');
+            },
+    
+            error: function(){
+                console.log("something went wrong on store.js");
+            },
+           });
+    
+      
+} 
+
+function test(args) {
+    alert('hello '+args+' well done');
+    
+}
