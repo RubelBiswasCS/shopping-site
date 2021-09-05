1dfc07aaae8bfceabb5479acd574bf3cb554a2db
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-09-05 19:58:05 +0600

    modified store.js

--- store/static/store/js/store.js
+++ store/static/store/js/store.js
@@ -10,52 +10,49 @@
 
 
 //fuction for add item to the cart
-var addToCart = function(args) {
-
-          
-           $.ajax({
-               method:"POST",
-               url: args.url,
-               data: args.data,
-               
-            success: function(response){
-                var myObj = JSON.parse(response);
-                console.log(myObj.user_id+' '+myObj.product_id);
-                console.log('data successfully send');
-            },
+$(".add-btn").on("click",function(e){
+       
+    // alert("add btn clicked");
+   
+    e.preventDefault();
+    var args = {
+        url: $(this).data("url"),
+        data: {
+            // csrfmiddlewaretoken : $(this).data("csrftoken"),
+            csrfmiddlewaretoken : csrf_token,
+            product_id : $(this).data("id"),
+        },
+    };
+    addToCart(args);
     
-            error: function(){
-                console.log("something went wrong on store.js");
-            },
-           });
-    
-      
-} 
+    // console.log($(this).data("csrftoken"));
+    // console.log(csrf_token);
+});
 
 
 //function for add to cart btn on product details
    
-    $("#add-btn2").on("click",function(e){
+    // $("#add-btn2").on("click",function(e){
        
-        // alert("add btn clicked");
+    //     // alert("add btn clicked");
        
-        e.preventDefault();
-        var args = {
-            url: $(this).data("url"),
-            data: {
-                // csrfmiddlewaretoken : $(this).data("csrftoken"),
-                csrfmiddlewaretoken : csrf_token,
-                product_id : $(this).data("id"),
-            },
-        };
-        addToCart2(args);
+    //     e.preventDefault();
+    //     var args = {
+    //         url: $(this).data("url"),
+    //         data: {
+    //             // csrfmiddlewaretoken : $(this).data("csrftoken"),
+    //             csrfmiddlewaretoken : csrf_token,
+    //             product_id : $(this).data("id"),
+    //         },
+    //     };
+    //     addToCart2(args);
         
-        // console.log($(this).data("csrftoken"));
-        // console.log(csrf_token);
-    });
+    //     // console.log($(this).data("csrftoken"));
+    //     // console.log(csrf_token);
+    // });
     // $(document).on("click","#add-btn2",addToCart2);
 //add to cart2
-    var addToCart2 = function(args) {
+    var addToCart = function(args) {
 
         $.ajax({
             method:"POST",
