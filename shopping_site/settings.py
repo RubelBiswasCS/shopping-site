8458bd7c467724985ebcf6a63287a9c5392bd02b
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-09-26 20:16:41 +0600

    added password reset feature

--- shopping_site/settings.py
+++ shopping_site/settings.py
@@ -141,8 +141,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
+
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.email.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('DEV_EMAIL')
